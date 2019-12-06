d6ec704ec761f5d3a46516d600e8c415dc2f0522
Carla <carligir@gmail.com>
2019-12-06 11:41:34 -0300

    Axios primer intento

--- js/generarUsuario.js
+++ js/generarUsuario.js
@@ -5,12 +5,14 @@
 // *La api de randomuser nos da mucha mas data como por ejemplo 3 imagenes, entonces cuando el POST nos de OK deberíamos mostrar la data del usuario que acabamos de CREAR y la imagen (obtenida en la primer llamada)
 
 // Declaración de variables
-const urlCrearUser = 'https://randomuser.me/api/' // En esta api se generan los datos del nuevo usuario
-const urlPostearUser = 'https://reqres.in/api/users' // En esta api se postean los datos del nuevo usuario
+var axios = require('axios');
+
+const urlCrearUser = 'https://randomuser.me/api/'; // En esta api se generan los datos del nuevo usuario
+const urlPostearUser = 'https://reqres.in/api/users'; // En esta api se postean los datos del nuevo usuario
 let botonGenerarUsuario = document.getElementById('botonGenerarUsuario');
-let main = document.getElementById('main');
+// let main = document.getElementById('main');
 let datosDelUsuario = document.getElementById('datosDelUsuario');
-let textoSucces = document.getElementById('textoSucces');
+let textoSuccess = document.getElementById('textoSuccess');
 let datosMostrados = document.getElementById('datosMostrados');
 let datosAMostrar = ['Nombre:','Apellido:','País:','Ciudad:','Email:','Usuario:','Contraseña:'];
 let datos;
@@ -28,12 +30,12 @@ function crearLi(padre, arregloARecorrer){
 
 // Lamados a la api
 
-async function crearUsuario(){
-    try {
-        const response = await fetch(urlCrearUser);
-        const user = await response.json();
+function crearUsuario(){
+    axios.get(urlCrearUser)
+    .then(user => {
+        console.log('user', user);
         nuevoUsuario = user.results[0];
-
+    
         datos = [nuevoUsuario.name.first,
                 nuevoUsuario.name.last,
                 nuevoUsuario.location.country,
@@ -41,26 +43,22 @@ async function crearUsuario(){
                 nuevoUsuario.email,
                 nuevoUsuario.login.username,
                 nuevoUsuario.login.password];
-    } catch (e) {
-        console.log('Error: ', e);
-    }
+    })
+    .catch(e => console.log('Error:', e));
 }
 
-async function postearUsuario(usuario){
-    try{
-        const response = await fetch(urlPostearUser, {
-            'name': `${usuario.name.first}`,
-            'job': `${usuario.location.city}`
-        });
-        const data = await response.json();
+function postearUsuario(usuario){
+    axios.post(urlPostearUser, {
+        'name': `${usuario.name.first}`,
+        'job': `${usuario.location.city}`
+    })
+    .then(data => {
         console.log('data', data.createdAt); // Esto no lo tengo que usar para nada?
-
         textoSuccess.innerHTML = 'Se ha generado un nuevo usuario:';
         crearLi(datosMostrados, datosAMostrar);
         crearLi(datosDelUsuario, datos);
-    } catch (e){
-        console.log('Error: ', e);
-    }
+    })
+    .catch (e => console.log('Error: ', e));
 }
 
 // EventListeners que ejecutan las funciones declaradas arriba
@@ -69,50 +67,3 @@ botonGenerarUsuario.addEventListener('click', ()=>{
     crearUsuario();
     postearUsuario(nuevoUsuario);
 })
-
-
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-// Funciones sin refactorizar:
-
-// function crearUsuario(){
-    //     fetch(urlCrearUser)
-    //     .then(response => response.text())
-    //     .then(content => {
-    //         console.log('JSON', JSON.parse(content)); // Devuelve un arreglo con un usuario
-    //         let usuarioGenerado = JSON.parse(content);
-    //         usuarioGenerado.results.forEach(user => nuevoUsuario = user);
-    //         datos = [nuevoUsuario.name.first, nuevoUsuario.name.last, nuevoUsuario.location.country, nuevoUsuario.location.city, nuevoUsuario.email, nuevoUsuario.login.username, nuevoUsuario.login.password];
-    //         return datos,nuevoUsuario;
-    //     })
-    //     .catch(error => console.log('Error', error));
-    // }
-
-    // function postearUsuario(usuario){
-        //     fetch(urlPostearUser, {
-        //         name: usuario.name.first,
-        //         job: usuario.location.city
-        //     })
-        //     .then(response => response.text())
-        //     .then(content => {
-        //         console.log('JSON.parse', JSON.parse(content));
-        //         textoSucces.innerHTML = 'Se ha generado un nuevo usuario:';
-        //         datosAMostrar.forEach(dato => {
-        //             let li = document.createElement('li');
-        //             datosMostrados.appendChild(li);
-        //             li.innerHTML = dato;
-        //         });
-        //         datos.forEach(dato => {
-        //             let li = document.createElement('li');
-        //             datosDelUsuario.appendChild(li);
-        //             li.innerHTML = dato;
-        //         });
-        //     })
-        //     .catch(error => console.log('Error', error));
-        // }
-        
